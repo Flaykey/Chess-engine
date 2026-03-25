75d0680ae9c9a6aa6d6a20234669a9c28fbcb2a0
Flaykey <samratghimire13@gmail.com>
2026-03-25 20:04:13 +0545

    Starting from scratch

--- include/assets.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<raylib.h>
-
-extern Texture2D dSquare,lSquare,altDSquare,altLSquare;
-extern Texture2D wPawn,wRook,wKnight,wBishop,wKing,wQueen,bPawn,bRook,bKnight,bBishop,bKing,bQueen;
-
-void LoadTex();
-void UnloadTex();
-
--- main.c
+++
@@ -1,8 +0,0 @@
-#include"include/game.h"
-
-int main(){
-    InitGame(1024,1024,"chess");
-    RunGame();
-    CloseGame();
-    return 0;
-}
